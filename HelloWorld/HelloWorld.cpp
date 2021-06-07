4c340480b094b8a15b396c4ca837bb3c89a1ba1e
xiix-crw <charles@loneaspen.com>
2021-06-07 07:07:52 -0700

    more comments s

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello World - It's a beautiful morning!\n";
     std::cout << "A horse left road apples in my driveway last night.\n";
-    std::cout << "I shoveled it up and dumped in the pile.\n";
+    std::cout << "I shoveled it up and dumped in the horse dung pile.\n";
     int i;
     std::cin >> i;
 }
